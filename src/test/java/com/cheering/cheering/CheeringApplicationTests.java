15a9f7a42f88117df9248bd093619b3d265f9233
whereami2048 <whereami2048@gmail.com>
2024-01-05 16:33:26 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/cheering/cheering/CheeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheering.cheering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
