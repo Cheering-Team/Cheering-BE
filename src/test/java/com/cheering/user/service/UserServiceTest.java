5fa730b1c360fa776d596c11ce8efbbcb1c87034
whereami0404 <whereami2048@gmail.com>
2024-01-09 04:02:34 +0900

    test: UserServiceTest 클래스 구현(로그인 기능 테스트 구현 필요)
    ==구현 테스트 목록==
    1. 이메일 검증 성공 테스트
    2. 이메일 유효성 테스트
    3. 이메일 중복 검증 테스트

---
+++ src/test/java/com/cheering/user/service/UserServiceTest.java
@@ -0,0 +1,85 @@
+package com.cheering.user.service;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.cheering.global.exception.user.DuplicatedEmailException;
+import com.cheering.user.domain.User;
+import com.cheering.user.domain.UserRepository;
+import com.cheering.user.dto.SignUpRequest;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class UserServiceTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserService userService;
+
+    @Test
+    @Transactional
+    void 회원가입_성공_테스트() {
+        //given
+        SignUpRequest signUpUser = new SignUpRequest("cheering@naver.com", "123456789", "nickname");
+
+        //when
+        User signup = userService.signup(signUpUser);
+        Optional<User> findUser = userRepository.findById(signup.getId());
+
+        //then
+        assertThatCode(() -> {
+            User user = findUser.orElseThrow();
+
+        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cheering@konkuk.ac.kr", "cheering@google.com"})
+    void 이메일_검증_성공_테스트(String rightEmail) {
+        //given
+
+        //when
+        //then
+        assertThatCode(() -> {
+            userService.validateEmail(rightEmail);
+        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cheeringNaver.com", "cheering@navercom", "cheering@naver.ac.kr.kr"})
+    void 이메일_유효성_예외_테스트(String wrongEmail) {
+        assertThatThrownBy(() -> {
+            userService.validateEmail(wrongEmail);
+        });
+    }
+
+    @Test
+    @Transactional
+    void 이메일_중복_예외_테스트() {
+        //given
+        SignUpRequest signUpUser = new SignUpRequest("cheering@naver.com", "123456789", "nickname");
+        User signup = userService.signup(signUpUser);
+
+        //when
+        String newEmail = "cheering@naver.com";
+
+        //then
+        assertThatThrownBy(() -> {
+            userService.validateEmail(newEmail);
+        }).isInstanceOf(DuplicatedEmailException.class);
+    }
+
+    @Test
+    void login() {
+    }
+}
