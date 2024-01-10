9bf83acc531cddb3a02e31d2fbebaa904184657c
whereami0404 <whereami2048@gmail.com>
2024-01-10 15:46:11 +0900

    feat: 회원가입 용 응답 추가

--- src/main/java/com/cheering/global/dto/ResponseGenerator.java
+++ src/main/java/com/cheering/global/dto/ResponseGenerator.java
@@ -1,11 +1,19 @@
 package com.cheering.global.dto;
 
 import com.cheering.auth.constant.JwtConstant;
+import com.cheering.auth.jwt.JWToken;
 import com.cheering.global.constant.ExceptionMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public interface ResponseGenerator {
+
+    static <T> ResponseEntity<ResponseBodyDto<?>> signUpSuccess(JWToken token, String message, T data) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Access-Token", JwtConstant.GRANT_TYPE + " " + token.accessToken())
+                .header("Refresh-Token", JwtConstant.GRANT_TYPE + " " + token.refreshToken())
+                .body(ResponseBodyDto.of(message, data));
+    }
 
     static <T> ResponseEntity<ResponseBodyDto<?>> success(String accessToken, String message, T data) {
         return ResponseEntity.status(HttpStatus.OK)
