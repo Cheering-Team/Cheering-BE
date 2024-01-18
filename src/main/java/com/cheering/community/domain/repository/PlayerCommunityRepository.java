451ae8e7a9a503b7e0e3c116b68c7d2c342ba5fa
whereami0404 <whereami2048@gmail.com>
2024-01-18 19:58:34 +0900

    feat: 커뮤니티 관련 Repository 인터페이스 구현

---
+++ src/main/java/com/cheering/community/domain/repository/PlayerCommunityRepository.java
@@ -0,0 +1,9 @@
+package com.cheering.community.domain.repository;
+
+import com.cheering.community.domain.PlayerCommunity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerCommunityRepository extends JpaRepository<PlayerCommunity, Long> {
+
+
+}
---
+++ src/main/java/com/cheering/community/domain/repository/TeamCommunityRepository.java
@@ -0,0 +1,7 @@
+package com.cheering.community.domain.repository;
+
+import com.cheering.community.domain.TeamCommunity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamCommunityRepository extends JpaRepository<TeamCommunity, Long> {
+}
