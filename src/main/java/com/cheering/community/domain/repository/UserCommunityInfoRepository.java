136dd378e4adf7f9923caf9b21c277383e7d0b15
whereami2048 <whereami2048@gmail.com>
2024-01-20 21:38:21 +0900

    feat: 중복 커뮤니티 가입 예외 처리

--- src/main/java/com/cheering/community/domain/repository/UserCommunityInfoRepository.java
+++ src/main/java/com/cheering/community/domain/repository/UserCommunityInfoRepository.java
@@ -1,8 +1,12 @@
 package com.cheering.community.domain.repository;
 
+import com.cheering.community.domain.Community;
 import com.cheering.community.domain.UserCommunityInfo;
+import com.cheering.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCommunityInfoRepository extends JpaRepository<UserCommunityInfo, Long> {
+
+    boolean existsByUserAndCommunity(User user, Community community);
 
 }
