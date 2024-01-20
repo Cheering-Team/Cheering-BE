78f87d350bcf26e7adca2e12e38b6d914af9b166
whereami0404 <whereami2048@gmail.com>
2024-01-20 20:19:26 +0900

    chore: 커뮤니티 데이터 세팅 내용 변경

--- src/main/java/com/cheering/community/service/CommunityService.java
+++ src/main/java/com/cheering/community/service/CommunityService.java
@@ -102,17 +102,18 @@ public class CommunityService {
 
     @Transactional
     public void setData() {
-        PlayerCommunity playerCommunity1 = PlayerCommunity.builder().name("playerA1")
+        PlayerCommunity playerCommunity1 = PlayerCommunity.builder().name("이강인")
                 .fanCount(1L).build();
-        PlayerCommunity playerCommunity2 = PlayerCommunity.builder().name("playerA2")
+        PlayerCommunity playerCommunity2 = PlayerCommunity.builder().name("음바페")
                 .fanCount(2L).build();
-        PlayerCommunity playerCommunity3 = PlayerCommunity.builder().name("playerA3")
+        PlayerCommunity playerCommunity3 = PlayerCommunity.builder().name("아센시오")
                 .fanCount(3L).build();
-        PlayerCommunity playerCommunity4 = PlayerCommunity.builder().name("playerB1")
+
+        PlayerCommunity playerCommunity4 = PlayerCommunity.builder().name("손흥민")
                 .fanCount(4L).build();
-        PlayerCommunity playerCommunity5 = PlayerCommunity.builder().name("playerB2")
+        PlayerCommunity playerCommunity5 = PlayerCommunity.builder().name("히샬리송")
                 .fanCount(5L).build();
-        PlayerCommunity playerCommunity6 = PlayerCommunity.builder().name("playerB3")
+        PlayerCommunity playerCommunity6 = PlayerCommunity.builder().name("메디슨")
                 .fanCount(6L).build();
 
         playerCommunityRepository.save(playerCommunity1);
@@ -122,37 +123,36 @@ public class CommunityService {
         playerCommunityRepository.save(playerCommunity5);
         playerCommunityRepository.save(playerCommunity6);
 
-        TeamCommunity teamCommunity1 = TeamCommunity.builder().name("teamCommunity1").players(new ArrayList<>())
+        TeamCommunity psgCommunity = TeamCommunity.builder().name("파리 생제르맹").players(new ArrayList<>())
                 .build();
-        TeamCommunity teamCommunity2 = TeamCommunity.builder().name("teamCommunity2").players(new ArrayList<>())
+        TeamCommunity tottenhamCommunity = TeamCommunity.builder().name("토트넘").players(new ArrayList<>())
                 .build();
 
-        teamCommunityRepository.save(teamCommunity1);
-        teamCommunityRepository.save(teamCommunity2);
+        teamCommunityRepository.save(psgCommunity);
+        teamCommunityRepository.save(tottenhamCommunity);
 
-        Player playerA1 = Player.builder().playerCommunity(playerCommunity1).name("playerA1").build();
-        Player playerA2 = Player.builder().playerCommunity(playerCommunity2).name("playerA2").build();
-        Player playerA3 = Player.builder().playerCommunity(playerCommunity3).name("playerA3").build();
+        Player playerA1 = Player.builder().playerCommunity(playerCommunity1).name("이강인").build();
+        Player playerA2 = Player.builder().playerCommunity(playerCommunity2).name("음바페").build();
+        Player playerA3 = Player.builder().playerCommunity(playerCommunity3).name("아센시오").build();
 
-        playerA1.connectTeamCommunity(teamCommunity1);
-        playerA2.connectTeamCommunity(teamCommunity1);
-        playerA3.connectTeamCommunity(teamCommunity1);
+        playerA1.connectTeamCommunity(psgCommunity);
+        playerA2.connectTeamCommunity(psgCommunity);
+        playerA3.connectTeamCommunity(psgCommunity);
 
         playerRepository.save(playerA1);
         playerRepository.save(playerA2);
         playerRepository.save(playerA3);
 
-        Player playerB1 = Player.builder().playerCommunity(playerCommunity4).name("playerB1").build();
-        Player playerB2 = Player.builder().playerCommunity(playerCommunity5).name("playerB2").build();
-        Player playerB3 = Player.builder().playerCommunity(playerCommunity6).name("playerB3").build();
+        Player playerB1 = Player.builder().playerCommunity(playerCommunity4).name("손흥민").build();
+        Player playerB2 = Player.builder().playerCommunity(playerCommunity5).name("히샬리송").build();
+        Player playerB3 = Player.builder().playerCommunity(playerCommunity6).name("메디슨").build();
 
-        playerB1.connectTeamCommunity(teamCommunity2);
-        playerB2.connectTeamCommunity(teamCommunity2);
-        playerB3.connectTeamCommunity(teamCommunity2);
+        playerB1.connectTeamCommunity(tottenhamCommunity);
+        playerB2.connectTeamCommunity(tottenhamCommunity);
+        playerB3.connectTeamCommunity(tottenhamCommunity);
 
         playerRepository.save(playerB1);
         playerRepository.save(playerB2);
         playerRepository.save(playerB3);
-
     }
 }
