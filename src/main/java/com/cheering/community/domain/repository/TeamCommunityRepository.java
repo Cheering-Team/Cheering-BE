65ef97b074fea8593c2bc96e673c51c1ab853c03
whereami0404 <whereami2048@gmail.com>
2024-01-26 05:14:36 +0900

    feat: 팀 커뮤니티 리포지토리 구현

--- src/main/java/com/cheering/community/domain/repository/TeamCommunityRepository.java
+++ src/main/java/com/cheering/community/domain/repository/TeamCommunityRepository.java
@@ -2,7 +2,10 @@ package com.cheering.community.domain.repository;
 
 import com.cheering.community.domain.TeamCommunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TeamCommunityRepository extends JpaRepository<TeamCommunity, Long> {
-    
+
+    TeamCommunity findByNameContainingIgnoreCase(@Param("name") String name);
+
 }
