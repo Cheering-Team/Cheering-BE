87aa2216f2c1e089d85c278d9e34b9bb2c13f13d
whereami0404 <whereami2048@gmail.com>
2024-01-25 19:13:51 +0900

    fix: 접근 권한 수정

--- src/main/java/com/cheering/auth/config/SecurityConfig.java
+++ src/main/java/com/cheering/auth/config/SecurityConfig.java
@@ -33,9 +33,13 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         // 해당 API에 대해서는 요청을 허가
-//                        .requestMatchers("/api/communities").hasRole("USER")
+                        .requestMatchers("/api/communities/**").hasRole("MEMBER")
+                        .requestMatchers("/api/users/**").hasRole("MEMBER")
                         // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
-                        .anyRequest().permitAll())
+                        .requestMatchers("/api/signup").permitAll()
+                        .requestMatchers("/api/signin").permitAll()
+                        .requestMatchers("/api/set-data").permitAll())
+//                        .anyRequest().permitAll())
                 .exceptionHandling(authenticationEntryPoint -> authenticationEntryPoint
                         .authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
