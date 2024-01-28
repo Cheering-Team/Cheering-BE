f696b44317ba6004bd4a1363b2a9ce91962767b6
whereami0404 <whereami2048@gmail.com>
2024-01-28 17:06:41 +0900

    feat: 커뮤니티 응답 DTO 참여 여부 추가

--- src/main/java/com/cheering/community/dto/response/CommunityResponse.java
+++ src/main/java/com/cheering/community/dto/response/CommunityResponse.java
@@ -1,5 +1,6 @@
 package com.cheering.community.dto.response;
 
+import com.cheering.community.constant.BooleanType;
 import com.cheering.community.domain.Community;
 import java.util.List;
 
@@ -7,15 +8,16 @@ public record CommunityResponse(
         Long id,
         String name,
         String image,
-        Long fanCount
+        Long fanCount,
+        BooleanType isJoin
 ) {
-    public static CommunityResponse of(Community community) {
+    public static CommunityResponse of(Community community, BooleanType isJoin) {
         return new CommunityResponse(community.getId(), community.getName(), community.getImage(),
-                community.getFanCount());
+                community.getFanCount(), isJoin);
     }
 
-    public static List<CommunityResponse> ofList(List<Community> playerCommunities) {
+    public static List<CommunityResponse> ofList(List<Community> playerCommunities, BooleanType isJoin) {
         return playerCommunities.stream().map(com ->
-                new CommunityResponse(com.getId(), com.getName(), com.getImage(), com.getFanCount())).toList();
+                new CommunityResponse(com.getId(), com.getName(), com.getImage(), com.getFanCount(), isJoin)).toList();
     }
 }
