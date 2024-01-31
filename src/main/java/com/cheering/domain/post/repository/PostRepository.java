50acb973583f0ca23e957a2fdc8ed0bed18fc136
whereami0404 <whereami2048@gmail.com>
2024-02-01 02:48:14 +0900

    feat: PostRepository 검색 기능 추가

--- src/main/java/com/cheering/domain/post/repository/PostRepository.java
+++ src/main/java/com/cheering/domain/post/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.cheering.domain.post.repository;
 
 import com.cheering.domain.post.domain.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByCommunityAndPlayer(Long communityId, Long writerId);
+
+    List<Post> findByCommunityAndUser(Long communityId, Long writerId);
 }
