e32c64468e174eb82b76b85acc44411e3463ce05
whereami0404 <whereami2048@gmail.com>
2024-02-01 04:01:24 +0900

    fix: 게시글 조회 응답 DTO 스펙 추가

--- src/main/java/com/cheering/domain/post/dto/PostResponse.java
+++ src/main/java/com/cheering/domain/post/dto/PostResponse.java
@@ -1,12 +1,17 @@
 package com.cheering.domain.post.dto;
 
 import com.cheering.domain.post.domain.Post;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record PostResponse(Long id,
-                           String content) {
+                           String content,
+                           LocalDateTime createdAt,
+                           LocalDateTime updatedAt,
+                           UserResponse owner
+) {
 
     public static List<PostResponse> ofList(List<Post> posts) {
         return posts.stream().map(post -> PostResponse.builder()
