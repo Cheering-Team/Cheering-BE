ae0141f26d849b159fa11debbf9176365819a153
whereami0404 <whereami2048@gmail.com>
2024-02-01 03:12:01 +0900

    feat: PostResponse DTO 변환 메소드 추가

--- src/main/java/com/cheering/domain/post/domain/Post.java
+++ src/main/java/com/cheering/domain/post/domain/Post.java
@@ -16,12 +16,14 @@ import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Post extends BaseEntity {
     @Id
     @GeneratedValue
--- src/main/java/com/cheering/domain/post/dto/PostResponse.java
+++ src/main/java/com/cheering/domain/post/dto/PostResponse.java
@@ -1,8 +1,17 @@
 package com.cheering.domain.post.dto;
 
+import com.cheering.domain.post.domain.Post;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record PostResponse(Long id,
                            String content) {
+
+    public static List<PostResponse> ofList(List<Post> posts) {
+        return posts.stream().map(post -> PostResponse.builder()
+                .id(post.getId())
+                .content(post.getContent())
+                .build()).toList();
+    }
 }
