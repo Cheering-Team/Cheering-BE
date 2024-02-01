35bed9cf9663c8776badcdb43daaf780c4dd3730
whereami0404 <whereami2048@gmail.com>
2024-02-01 16:16:29 +0900

    feat: 게시글 조회 응답 DTO 스펙 변경

--- src/main/java/com/cheering/domain/post/dto/PostResponse.java
+++ src/main/java/com/cheering/domain/post/dto/PostResponse.java
@@ -1,6 +1,7 @@
 package com.cheering.domain.post.dto;
 
 import com.cheering.domain.post.domain.Post;
+import com.cheering.domain.user.dto.response.PostOwnerResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -8,15 +9,23 @@ import lombok.Builder;
 @Builder
 public record PostResponse(Long id,
                            String content,
+                           Long commentCount,
+                           Long likeCount,
                            LocalDateTime createdAt,
                            LocalDateTime updatedAt,
-                           UserResponse owner
+                           PostOwnerResponse owner
 ) {
 
-    public static List<PostResponse> ofList(List<Post> posts) {
+    public static List<PostResponse> ofList(List<Post> posts, PostOwnerResponse owner) {
         return posts.stream().map(post -> PostResponse.builder()
                 .id(post.getId())
                 .content(post.getContent())
-                .build()).toList();
+                .commentCount(100L)
+                .likeCount(200L)
+                .owner(owner)
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .build()
+        ).toList();
     }
 }
