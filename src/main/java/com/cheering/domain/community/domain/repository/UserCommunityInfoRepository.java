0d499df2441aabb9c81759ed6d37924fa0a0006c
whereami0404 <whereami2048@gmail.com>
2024-02-01 16:02:12 +0900

    feat: 유저 커뮤니티 정보 Repository 조회 기능 추가

--- src/main/java/com/cheering/domain/community/domain/repository/UserCommunityInfoRepository.java
+++ src/main/java/com/cheering/domain/community/domain/repository/UserCommunityInfoRepository.java
@@ -4,11 +4,14 @@ import com.cheering.domain.community.domain.Community;
 import com.cheering.domain.community.domain.UserCommunityInfo;
 import com.cheering.domain.user.domain.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCommunityInfoRepository extends JpaRepository<UserCommunityInfo, Long> {
 
     List<UserCommunityInfo> findByUser(User user);
+
+    Optional<UserCommunityInfo> findByUserAndCommunity(User user, Community community);
 
     boolean existsByUserAndCommunity(User user, Community community);
 
