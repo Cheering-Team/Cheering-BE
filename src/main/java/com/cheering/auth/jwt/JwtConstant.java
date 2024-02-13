36e9a5e0faccec4f95ce7642de0c6f45a34d4ed0
whereami2048 <whereami2048@gmail.com>
2024-02-13 13:31:47 +0900

    chore: 토큰 만료시간 변경

--- src/main/java/com/cheering/auth/jwt/JwtConstant.java
+++ src/main/java/com/cheering/auth/jwt/JwtConstant.java
@@ -1,9 +1,9 @@
 package com.cheering.auth.jwt;
 
 public abstract class JwtConstant {
-    // * 60 * 60 * 24
-    public static final Long ACCESS_TOKEN_EXPIRE_TIME = 1000L;
-    public static final Long REFRESH_TOKEN_EXPIRE_TIME = 1000L;
+    // 1000L * 60 * 60 == 1h
+    public static final Long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60;
+    public static final Long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24;
     public static final String GRANT_TYPE = "Bearer";
     public static final String ACCESS_TOKEN_KEY_NAME = "Access-Token";
     public static final String REFRESH_TOKEN_KEY_NAME = "Refresh-Token";
