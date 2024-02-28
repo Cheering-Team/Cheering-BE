ab9841729c5779ebae807e95905b22e4d7390bca
whereami2048 <whereami2048@gmail.com>
2024-02-29 03:48:50 +0900

    chore: 채팅 기능 테스트

--- src/main/java/com/cheering/chat/config/RabbitConfig.java
+++ src/main/java/com/cheering/chat/config/RabbitConfig.java
@@ -15,8 +15,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @EnableRabbit
 public class RabbitConfig {
 
