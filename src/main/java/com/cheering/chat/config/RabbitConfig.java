39a94e76c75a425cb188173f2d39429766635d3a
whereami2048 <whereami2048@gmail.com>
2024-03-01 02:21:52 +0900

    chore: 배포 테스트

--- src/main/java/com/cheering/chat/config/RabbitConfig.java
+++ src/main/java/com/cheering/chat/config/RabbitConfig.java
@@ -69,7 +69,7 @@ public class RabbitConfig {
         factory.setHost("3.37.244.109");
         factory.setUsername("guest");
         factory.setPassword("guest");
-        factory.setPort(61613);
+        factory.setPort(5672);
 
         return factory;
     }
