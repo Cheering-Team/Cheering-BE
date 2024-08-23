8b39b067b1573ce662452b31e446d40b2224373c
Junseo <monstazo911@gmail.com>
2024-08-23 16:56:26 +0900

    feat: 신고 누적 답글 숨김 처리

--- src/main/java/com/cheering/comment/reComment/ReCommentController.java
+++ src/main/java/com/cheering/comment/reComment/ReCommentController.java
@@ -25,6 +25,7 @@ public class ReCommentController {
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.CREATED, "답글이 작성되었습니다.", reCommentService.writeReComment(commentId, requestDTO, customUserDetails.getUser())));
     }
 
+    // 특정 댓글의 답글 불러오기
     @GetMapping("/comments/{commentId}/re")
     public ResponseEntity<?> getReComments(@PathVariable("commentId") Long commentId, @AuthenticationPrincipal CustomUserDetails customUserDetails){
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, "답글들을 불러왔습니다.", reCommentService.getComments(commentId, customUserDetails.getUser())));
@@ -35,25 +36,4 @@ public class ReCommentController {
         reCommentService.deleteReComment(reCommentId, customUserDetails.getUser());
         return ResponseEntity.ok().body(ApiUtils.success(HttpStatus.OK, "답글이 삭제되었습니다.", null));
     }
-
-//    @PostMapping("/communities/{communityId}/posts/{postId}/comments/{commentId}/recomments")
-//    public ResponseEntity<ResponseBodyDto<?>> createReComment(@PathVariable("communityId") Long communityId,
-//                                                              @PathVariable("postId") Long postId,
-//                                                              @PathVariable("commentId") Long commentId,
-//                                                              @RequestBody CommentRequest commentRequest) {
-//
-//        Long newReCommentId = reCommentService.createReComment(communityId, postId, commentId,
-//                commentRequest.content());
-//
-//        return ResponseGenerator.success(SuccessMessage.CREATE_RE_COMMENT_SUCCESS, newReCommentId);
-//    }
-
-//    @GetMapping("/communities/{communityId}/posts/{postId}/comments/{commentId}/recomments")
-//    public ResponseEntity<ResponseBodyDto<?>> getReComments(@PathVariable("communityId") Long communityId,
-//                                                            @PathVariable("postId") Long postId,
-//                                                            @PathVariable("commentId") Long commentId) {
-//        List<ReCommentResponse> findReComments = reCommentService.getReComments(commentId);
-//
-//        return ResponseGenerator.success(SuccessMessage.GET_RE_COMMENT_SUCCESS, findReComments);
-//    }
 }
