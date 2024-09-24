180e698535b7c5920ead58d3dad39a7d1e89707a
Junseo <monstazo911@gmail.com>
2024-09-24 12:20:19 +0900

    fix: 토큰 로테이션 후 로그아웃 시 에러 해결

--- src/main/java/com/cheering/user/UserService.java
+++ src/main/java/com/cheering/user/UserService.java
@@ -102,7 +102,6 @@ public class UserService {
 
     @Transactional
     public UserResponse.TokenDTO refresh(String refreshToken) {
-        System.out.println("토큰 재발급");
         String token = refreshToken.split(" ")[1];
 
         String phone = jwtUtil.getUsername(token);
@@ -116,17 +115,12 @@ public class UserService {
             throw new CustomException(ExceptionCode.INVALID_TOKEN);
         }
 
-//        String accessToken = jwtUtil.createJwt(phone, role, 1000 * 60 * 60 * 24L);
-//        refreshToken = jwtUtil.createJwt(phone, role, 1000 * 60 * 60 * 24 * 30L);
-
-        String accessToken = jwtUtil.createJwt(phone, role, 1000 * 60L);
-        refreshToken = jwtUtil.createJwt(phone, role, 1000 * 60 * 2L);
+        String accessToken = jwtUtil.createJwt(phone, role, 1000 * 60 * 60 * 24L);
+        refreshToken = jwtUtil.createJwt(phone, role, 1000 * 60 * 60 * 24 * 30L);
 
         redisUtils.deleteData(user.getId().toString());
-//        redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 60 * 24 * 30L);
-        redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 2L);
+        redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 60 * 24 * 30L);
 
-        System.out.println("토큰 재발급 완료");
         return new UserResponse.TokenDTO(accessToken, refreshToken);
     }
 
@@ -176,15 +170,11 @@ public class UserService {
 
         if(optionalUser.isPresent()) {
             User user = optionalUser.get();
-//            String accessToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60 * 60 * 24L);
-//            String refreshToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60 * 60 * 24 * 30L);
-
-            String accessToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60L);
-            String refreshToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60 * 2L);
+            String accessToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60 * 60 * 24L);
+            String refreshToken = jwtUtil.createJwt(user.getPhone(), user.getRole().getValue(), 1000 * 60 * 60 * 24 * 30L);
 
             redisUtils.deleteData(user.getId().toString());
-//            redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 60 * 24 * 30L);
-            redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 2L);
+            redisUtils.setDataExpire(user.getId().toString(), refreshToken, 1000 * 60 * 60 * 24 * 30L);
 
             return new UserResponse.TokenDTO(accessToken, refreshToken);
         } else {
