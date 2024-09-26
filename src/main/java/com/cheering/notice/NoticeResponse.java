20ae583725c1b70526d635e6d0b41886dec5339f
Junseo <monstazo911@gmail.com>
2024-09-26 13:44:19 +0900

    feat: 공지사항 응답 수정

--- src/main/java/com/cheering/notice/NoticeResponse.java
+++ src/main/java/com/cheering/notice/NoticeResponse.java
@@ -3,9 +3,9 @@ package com.cheering.notice;
 import java.time.LocalDateTime;
 
 public class NoticeResponse {
-    public record NoticeDTO (Long id, String title, LocalDateTime createdAt) {
+    public record NoticeDTO (Long id, String title, String image, String content, LocalDateTime createdAt) {
         public NoticeDTO(Notice notice) {
-            this(notice.getId(), notice.getTitle(), notice.getCreatedAt());
+            this(notice.getId(), notice.getTitle(), notice.getImage(), notice.getContent(), notice.getCreatedAt());
         }
     }
 }
