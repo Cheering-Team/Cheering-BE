6e5ea3e45f1b774deebf20e770dca7af390956d3
Junseo <monstazo911@gmail.com>
2024-10-24 22:48:11 +0900

    fix: 채팅 불러오기

--- src/main/java/com/cheering/chat/ChatRepository.java
+++ src/main/java/com/cheering/chat/ChatRepository.java
@@ -15,6 +15,6 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     @Query("SELECT c FROM Chat c WHERE c.chatRoom = :chatRoom ORDER BY c.createdAt DESC")
     Page<Chat> findByChatRoom(@Param("chatRoom") ChatRoom chatRoom, Pageable pageable);
 
-    @Query(value = "SELECT * FROM chat_tb WHERE DATE_TRUNC('minute', created_at) = DATE_TRUNC('minute', CURRENT_TIMESTAMP) AND chat_room_id = :chatRoomId AND player_user_id = :writerId", nativeQuery = true)
+    @Query(value = "SELECT * FROM chat_tb WHERE DATE_TRUNC('minute', created_at) = DATE_TRUNC('minute', CURRENT_TIMESTAMP) AND chat_room_id = :chatRoomId AND writer_id = :writerId", nativeQuery = true)
     Optional<Chat> findByChatRoomAndWriterAndCreatedAtMinute(@Param("chatRoomId") Long chatRoomId, @Param("writerId") Long writerId);
 }
