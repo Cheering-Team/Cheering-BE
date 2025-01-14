f719d2bb861cc81e1a140f746655b1d7d0b9f1ec
sam-mae <lsslsm2@pusan.ac.kr>
2025-01-14 10:55:41 +0900

    feat: MeetFanRole 수정

---
+++ src/main/resources/db/migration/V19__add_meet_fan_role_applier.sql
@@ -0,0 +1,11 @@
+ALTER TABLE meet_fan_tb
+    DROP CONSTRAINT meet_fan_tb_role_check;
+
+ALTER TABLE meet_fan_tb
+    ADD CONSTRAINT meet_fan_tb_role_check
+        CHECK ((role)::text = ANY
+               (ARRAY [
+                   ('MANAGER'::character varying)::text,
+                   ('MEMBER'::character varying)::text,
+                   ('APPLIER'::character varying)::text
+                   ]));
